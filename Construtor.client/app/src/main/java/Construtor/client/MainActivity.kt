6d41eef87d04be0068b2169e9075d2bcb2b104b1
Thomas (Void) <thomas_neves@ymail.com>
2024-11-13 10:11:46 -0300

    update main login
    configurações de progress bar e definição de espaço

--- Construtor.client/app/src/main/java/Construtor/client/MainActivity.kt
+++ Construtor.client/app/src/main/java/Construtor/client/MainActivity.kt
@@ -1,22 +1,39 @@
 package Construtor.client
 
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(LayoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        supportActionBar!!.hide()
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#FFFFFF")
+        binding.btEntrar.setOnClickListener {
+
+            Val email = binding.editEmail.text.toString()
+            Val senha = binding.editEmail.text.toString()
+
+            when{
+                email.isEmpty()
+            }
+        }
     }
 }
