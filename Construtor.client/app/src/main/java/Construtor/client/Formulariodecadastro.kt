afbbf68edbf6a4247708c717c4563a666a1202cc
Thomas Neves <thomas_neves@ymail.com>
2025-01-08 21:20:26 -0300

    update de form e tela inicial
    efetuada a configuração do layout e funcionalidade de voltar da tela de cadastro, Pendente: configurar cadastro em banco de dados.
    efetuada a configuração e criação dos botões das tela iniciais, Pendente: configurar a funcionalidades de cada botão.

---
+++ Construtor.client/app/src/main/java/Construtor/client/Formulariodecadastro.kt
@@ -0,0 +1,28 @@
+package Construtor.client
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Formulariodecadastro : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.formulario_de_cadastro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val textViewRedirect = findViewById<ImageView>(R.id.icon_back)
+        textViewRedirect.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
